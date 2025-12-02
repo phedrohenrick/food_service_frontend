4933db934576111264fb7914c28fa911d336c257
phedrohenrick <phedrohenrickdasilva@gmail.com>
2025-12-02 10:13:40 -0300

    refactor: deleta pastas sem uso

--- lp/src/apps/restaurant-dashboard-v2/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Dashboard } from './Dashboard';
-export { default as Orders } from './Orders';
-export { default as Menu } from './Menu';
-export { default as Settings } from './Settings';
